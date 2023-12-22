c4c82e9a6450ec4ec9462a533e1f1833a8a0ff25
tatialveso <tatiialveso@gmail.com>
2023-12-22 14:04:31 -0300

    :sparkles: setting navbar with sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import profileImg from '../assets/profile.png'
+import Sidebar from './Sidebar'
 
 function Navbar() {
     return (
-        <div className="bg-gray-800 w-screen flex justify-between items-center p-5">
-            <h1 className="text-white">Logo</h1>
-            <input
-                type="text"
-                placeholder="Buscar candidato"
-                className="rounded-3xl w-6/12 p-2"
-            />
-            <img className="rounded-full w-10 h-10" src={ profileImg } alt="" />
+        <div>
+            <Sidebar />
+            <div className="fixed bg-gray-800 w-screen flex justify-between items-center p-5 shadow-xl">
+                <h1 className="text-white">Logo</h1>
+                <input
+                    type="text"
+                    placeholder="Buscar candidato"
+                    className="rounded-3xl w-6/12 p-2"
+                />
+                <img className="rounded-full w-10 h-10" src={ profileImg } alt="" />
+            </div>
         </div>
     )
 }
