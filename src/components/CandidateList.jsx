5a7e37aac005ba8f37be39d899f2f4fd16374910
tatialveso <tatiialveso@gmail.com>
2023-12-22 14:10:51 -0300

    :lipstick: adding padding top

--- src/components/CandidateList.jsx
+++ src/components/CandidateList.jsx
@@ -2,7 +2,7 @@ import CandidateItem from "./CandidateItem";
 
 function CandidateList() {
   return (
-    <ul role="list" className="divide-y divide-gray-100 ml-48">
+    <ul role="list" className="divide-y divide-gray-100 ml-48 pt-24">
       <CandidateItem />
       <CandidateItem />
       <CandidateItem />
