0c17e09fe0dc080b6299e2f117e1df113ae65619
tatialveso <tatiialveso@gmail.com>
2023-12-27 19:07:16 -0300

    :recycled: fix linking in sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
 import profileImg from '../assets/profile.png'
 import Sidebar from './Sidebar'
 
@@ -7,12 +8,17 @@ function Navbar({ search, setSearch }) {
         <div>
             <Sidebar />
             <div className="fixed bg-gray-800 w-screen flex justify-between items-center p-5 shadow-xl">
-                <h1 className="text-white">Logo</h1>
+                <Link to="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-white w-10 h-10">
+                        <path fillRule="evenodd" d="M4.5 2.25a.75.75 0 0 0 0 1.5v16.5h-.75a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5h-.75V3.75a.75.75 0 0 0 0-1.5h-15ZM9 6a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H9Zm-.75 3.75A.75.75 0 0 1 9 9h1.5a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM9 12a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H9Zm3.75-5.25A.75.75 0 0 1 13.5 6H15a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75ZM13.5 9a.75.75 0 0 0 0 1.5H15A.75.75 0 0 0 15 9h-1.5Zm-.75 3.75a.75.75 0 0 1 .75-.75H15a.75.75 0 0 1 0 1.5h-1.5a.75.75 0 0 1-.75-.75ZM9 19.5v-2.25a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-4.5A.75.75 0 0 1 9 19.5Z" clipRule="evenodd" />
+                    </svg>
+
+                </Link>       
                 <input
                     type="text"
                     placeholder="Buscar por um candidato"
                     className="rounded-3xl w-6/12 p-2 pl-5"
-                    value={ search }
+                    value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
                 <img className="rounded-full w-10 h-10" src={profileImg} alt="" />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,30 @@
+import { Link } from 'react-router-dom'
+
 function Sidebar() {
     return (
-        <div className="pointer-events-none fixed inset-y-0 w-52">
+        <div className="fixed inset-y-0 w-52">
             <div className="flex h-screen flex-col bg-white py-6 shadow-xl">
                 <div className="py-20 sm:px-6">
-                    <p className="text-base font-semibold leading-6 text-gray-900">
-                        Panel title
-                    </p>
+                    <div className='flex flex-col gap-4'>
+                        <div className='flex gap-3 font-medium text-lg items-center uppercase hover:text-yellow-600'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path fillRule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                            </svg>
+                            <Link to="/">Todos</Link>
+                        </div>
+                        <div className='flex gap-3 font-medium text-lg items-center uppercase hover:text-yellow-600'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <Link to="/atendidos">Atendidos</Link>
+                        </div>
+                        <div className='flex gap-3 font-medium text-lg items-center uppercase hover:text-yellow-600'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                            <Link to="/lixeira">Lixeira</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
